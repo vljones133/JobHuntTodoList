9fcbe559028bac58e8f200d57c73f89b0bf3a7a6
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-07-30 12:06:56 -0500

    need to walk away, come back to homescreen keyextractor

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  FlatList,
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import styles from './styles';
+import { auth, db } from '../../firebase/config';
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  addDoc,
+  serverTimestamp,
+} from 'firebase/firestore';
 
-// export default function HomeScreen(props) {
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen(props) {
+  const q = query(collection(db, 'tasks'), orderBy('timestamp', 'desc'));
+
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+  useEffect(() => {
+    onSnapshot(q, (snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          item: doc.data(),
+        }))
+      );
+    });
+  }, [input]);
+  const addTask = (e) => {
+    e.preventDefault();
+    addDoc(collection(db, 'tasks'), {
+      task: input,
+      timestamp: serverTimestamp(),
+    });
+    setInput('');
+  };
+
+  const renderTask = ({ item, index }) => {
+    return (
+      <View style={styles.taskContainer}>
+        <Text style={styles.taskText}>
+          {index}. {item.text}
+        </Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add new task"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={(text) => setTasks(text)}
+          value={input}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button} onPress={addTask}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      {tasks && (
+        <View style={styles.listContainer}>
+          <FlatList
+            data={tasks}
+            renderItem={renderTask}
+            keyExtractor={(item) => item.id}
+            removeClippedSubviews={true}
+          />
+        </View>
+      )}
     </View>
   );
 }
